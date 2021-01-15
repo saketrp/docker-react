7e9ebe9bf026f118c2dd097b05139b9b966f2254
saketrp <saketrp@gmail.com>
2021-01-15 15:23:51 +0530

    added missed files

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-	  Come on!
-	  Love big butts
         </p>
         <a
           className="App-link"
